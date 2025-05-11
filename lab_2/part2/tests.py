f58df91be5ecd0bed215a6baf78393cd3ada3bd8
lizka19 <lizakolganowa@gmail.com>
2025-05-11 20:14:46 +0400

    add function same_bits_test

--- lab_2/part2/tests.py
+++ lab_2/part2/tests.py
@@ -3,6 +3,7 @@ sys.path.append(r'C:\Users\lizak\PycharmProjects\isb\lab_2')
 import math
 
 from work_files import *
+from scipy.special import gammainc
 
 
 def frequency_bit_test(sequence: str) -> float:
@@ -23,4 +24,31 @@ def frequency_bit_test(sequence: str) -> float:
     return p_value
 
 
+def same_bits_test(sequence: str) -> float:
+    """
+    Выполняет тест на идентичные последовательные биты.
+    :param sequence: Последовательность
+    :return: P-значение
+    """
+
+    p_value = 0
+
+    n = len(sequence)
+
+    p = sequence.count('1') / n
+
+    if abs(p - 0.5) >= 2 / math.sqrt(n):
+        return p_value
+
+    v_n = 0
+
+    for i in range(n - 1):
+
+        if sequence[i] != sequence[i + 1]:
+            v_n += 1
+
+    numerator = abs(v_n - 2 * n * p * (1 - p))
+    denominator = 2 * math.sqrt(2 * n) * p * (1 - p)
+
+    return math.erfc(numerator / denominator)
 
